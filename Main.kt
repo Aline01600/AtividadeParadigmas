53a20badf829e8c389f68c20ad1e88053e22742e
nandavicente <mafedevicente@gmail.com>
2025-06-17 22:27:19 -0300

    feat: Adiciona estratégia de filtragem de números pares e atualiza o main para demonstração

--- Main.kt
+++ Main.kt
@@ -1,5 +1,22 @@
+package manipulacaolistas
 fun main() {
     
+    val numerosOriginais = listOf(5, 2, 8, 1, 9, 4, 2, 7, 6, 10, 3) 
+    println("--- Lista Original ---")
+    println("Lista: $numerosOriginais")
+    println()
+
+    // --- 1. Estrategia: Ordenação Crescente ---
+    val listaCrescente = executar_estrategia(numerosOriginais, ::ordenarCrescente)
+    println("--- Ordenacao Crescente ---")
+    println("Resultado: $listaCrescente")
+    println()
+
+    // --- 4. Estratégia: Filtragem de Números Pares ---
+    val numerosPares = executar_estrategia(numerosOriginais, ::filtrarNumerosPares)
+    println("--- Filtragem de Numeros Pares ---")
+    println("Resultado: $numerosPares")
+    println()
 }
 
 fun executar_estrategia(lista: List<Int>, estrategia: (List<Int>) -> List<Int>): List<Int> {
